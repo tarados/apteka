dc89e3fbea161ab0810341b3c99501174af19a5a
«sergey» <lith2009@mail.ru>
2019-06-03 22:55:24 +0300

    settings postgresql

--- conf/settings.py
+++ conf/settings.py
@@ -74,19 +74,19 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'apteka',
-        'USER': 'sergey',
-        'PASSWORD': 'zvada1601',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'apteka',
+#         'USER': 'sergey',
+#         'PASSWORD': 'zvada1601',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
-# DATABASES = {'default': dj_database_url.config(default=env['DB_URL'])}
-# DATABASES['default']['CONN_MAX_AGE'] = None
+DATABASES = {'default': dj_database_url.config(default=env['DB_URL'])}
+DATABASES['default']['CONN_MAX_AGE'] = None
 
 # DATABASES = {
 #     'default': {
