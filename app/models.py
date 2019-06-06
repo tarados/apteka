84205584da240465281dd54840d99b03dfc95af5
«sergey» <lith2009@mail.ru>
2019-06-07 00:06:46 +0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
+class Farmacy(models.Model):
+    """Модель торговой точки"""
+
+    farmacy_id = models.IntegerField()
+    pharmacy_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    house = models.CharField(max_length=50)
+    state_province = models.CharField(max_length=50)
+    manager = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    """Модель прайс"""
+
+    class Meta:
+        db_table = 'product'
+
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=250)
+    product_manufacturer = models.CharField(max_length=250, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+
+
+class ProductAvailability(models.Model):
+    """Модель наличие товара"""
+
+    class Meta:
+        db_table = 'product_availability'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    farmacy = models.ForeignKey(Farmacy, on_delete=models.CASCADE)
+    number = models.FloatField(null=True, blank=True)
+
+
 # Create your models here.
