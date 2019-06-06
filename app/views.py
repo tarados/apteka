b12d8498a2abc2ed3b734b15f611067e90ef8417
«sergey» <lith2009@mail.ru>
2019-06-06 05:51:23 +0300

    delete file

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseNotFound, HttpRequest
+from django.shortcuts import render_to_response, redirect
+from django.template.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
+
+
+def index(request):
+    books = User.objects.all()
+    payload = []
+    for book in books:
+        payload.append({'id': book.id, 'name': book.username})
+    return JsonResponse({'payload': payload})
 # Create your views here.
