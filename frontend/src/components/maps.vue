f7c09acb2c7e17dbd0be57ae90e7a135fc26a2ba
sergey <lith2009@mail.ru>
2019-07-08 06:03:57 +0300

    with components

---
+++ frontend/src/components/maps.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <div id="myMap"></div>
+    </div>
+</template>
+
+<script>
+    import GoogleMapsLoader from 'google-maps'
+
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        mounted: function () {
+            GoogleMapsLoader.onLoad(function(google) {
+                console.log('I just loaded google maps api');
+            });
+        }
+
+    }
+</script>
+
+<style scoped>
+    #myMap {
+        height: 300px;
+        width: 100%;
+    }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
-//Vue.use(Bootstrap)
+import AppProduct from "./components/Product.vue";
+import AppFarmacy from "./components/Farmacy.vue";
+import AppMaps from "./components/maps.vue";
+
+Vue.component("AppPoduct", AppProduct);
+Vue.component("AppFarmacy", AppFarmacy);
+Vue.component("AppMaps", AppMaps);
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
