31794311b68404d735d9893008d4bb41dd9e2b04
sergey <lith2009@mail.ru>
2019-07-15 05:58:28 +0300

    for work

--- app/views.py
+++ app/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
 from app.models import Product
-from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseNotFound, HttpRequest
-from django.shortcuts import render_to_response, redirect
-from django.template.context_processors import csrf
-from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -24,4 +16,4 @@ def index(request):
             payload.append(
                 {'product': product.product_name, 'manufacturer': product.product_manufacturer, 'price': product.price})
     return JsonResponse({'payload': payload})
-# Create your views here.
+
