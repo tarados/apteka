c375d25ac4746cc5c25081a911f7b76acb4afac3
sergey <lith2009@mail.ru>
2020-01-20 21:04:52 +0300

    basket component change

--- frontend/src/templates/Basket.vue
+++ frontend/src/templates/Basket.vue
@@ -6,11 +6,20 @@
                     <i>Basket</i>
                 </div>
             </div>
-            <div id="contentBasket">
-                <div class="col product">sdfsd</div>
-                <div class="col quantity">erwew</div>
-                <div class="col total">wewe</div>
-                <div class="col deleteOrder">gerer</div>
+            <div class="contentBasket" v-for="(product, index) in productList" :key="index">
+                <div class="photo">
+                    <img :src="product.photo" :alt="product.title">
+                </div>
+                <div class="product">
+                    <div class="title" v-text="product.title"></div>
+                    <div class="manufacturer" v-text="product.manufacturer"></div>
+                    <div class="price" v-text="product.price + ' руб.'"></div>
+                </div>
+                <div class="quantity">quantity</div>
+                <div class="total">total</div>
+                <b-button-close class="deleteOrder" @click="deleteOrder(index)">
+                    <custom-icon name="x" class="custom-icon"/>
+                </b-button-close>
             </div>
             <div id="footer">
                 <b-button variant="success" @click="toHome">Button</b-button>
@@ -21,26 +30,41 @@
 </template>
 
 <script>
+    import customIcon from 'vue-icon/lib/vue-feather.esm'
+    import * as basket from '../basket'
+
     export default {
         name: "Basket",
+        components: {
+            // eslint-disable-next-line vue/no-unused-components
+            customIcon
+        },
+        data() {
+            return {
+                productList: []
+            }
+        },
         methods: {
             toHome() {
                 this.$router.go(-1);
             },
-            currentPage() {
-                // eslint-disable-next-line no-console
-                console.log(this.$router);
+            loadBasket() {
+                this.productList = basket.getItems();
+            },
+            deleteOrder(index) {
+                basket.deleteItem(index);
+                this.loadBasket();
             }
         },
         mounted() {
-            this.currentPage();
+            this.loadBasket();
         }
     }
 </script>
 
 <style scoped>
     .container {
-       border: 1px solid red;
+        border: 1px solid red;
     }
 
     .wrapper {
@@ -53,6 +77,7 @@
     #header {
         width: 100%;
         height: 3em;
+        margin-bottom: 5px;
         background-color: gold;
         display: flex;
         flex-wrap: wrap;
@@ -60,22 +85,66 @@
         justify-content: center
     }
 
-    #contentBasket {
+    .contentBasket {
         width: 100%;
-        min-height: 20px;
+        margin: 5px 0;
         background-color: #6DDCBD;
-        margin: 0 -15px;
+        display: grid;
+        grid-template-columns: 0.7fr 3fr 1fr 0.5fr 0.3fr;
     }
 
-    .wrapper .col {
+    .photo {
+        grid-column: 1 / 1;
+    }
+
+    img {
+        max-height: 6em;
+    }
+
+    .product {
+        grid-column: 2 / 2;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .title, .manufacturer, .price {
+        flex-grow: 0.5;
+        width: 100%;
         text-align: center;
-        border: 1px solid yellow;
-        margin: 5px;
-        width: calc(100% / 4 - 10px);
-        display: inline-block;
+    }
+
+    .manufacturer {
+
+    }
+
+    .price {
+
+    }
+
+    .quantity, .total {
+        place-self: center;
+    }
+
+    .quantity {
+        grid-column: 3 / 3;
+    }
+
+    .total {
+        grid-column: 4 / 4;
+    }
+
+    .deleteOrder {
+        grid-column: 5 / 5;
+        place-self: self-start;
+        margin-left: auto;
     }
 
     #footer {
-
+        margin: 10px auto;
     }
+
+    .custom-icon {
+        width: 24px;
+    }
+
 </style>
