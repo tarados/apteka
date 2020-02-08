897a1ea5b7f7c24c6f82621d015756c407ec9ac8
sergey <lith2009@mail.ru>
2020-02-08 09:55:42 +0300

    add vee-validate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 import BootstrapVue from 'bootstrap-vue'
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize
+} from "vee-validate";
+import en from "vee-validate/dist/locale/en.json";
+import * as rules from "vee-validate/dist/rules";
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import feather from 'vue-icon'
 
+// Install VeeValidate rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
 
+localize("en", en);
 
-Vue.config.productionTip = false;
+// Install VeeValidate components globally
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
 Vue.use(BootstrapVue);
 Vue.use(feather, 'v-icon');
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- frontend/src/templates/Login.vue
+++ frontend/src/templates/Login.vue
@@ -1,15 +1,89 @@
 <template>
     <div>
-        123
+        <validation-observer ref="observer" v-slot="{ passes }">
+            <b-form @submit.stop.prevent="passes(onSubmit)">
+                <validation-provider
+                        name="Name"
+                        :rules="{ required: true, min: 3 }"
+                        v-slot="validationContext"
+                >
+                    <b-form-group id="example-input-group-1" label="Name" label-for="example-input-1">
+                        <b-form-input
+                                id="example-input-1"
+                                name="example-input-1"
+                                v-model="form.name"
+                                :state="getValidationState(validationContext)"
+                                aria-describedby="input-1-live-feedback"
+                        ></b-form-input>
+
+                        <b-form-invalid-feedback id="input-1-live-feedback">{{ validationContext.errors[0] }}
+                        </b-form-invalid-feedback>
+                    </b-form-group>
+                </validation-provider>
+
+                <validation-provider name="Food" :rules="{ required: true }" v-slot="validationContext">
+                    <b-form-group id="example-input-group-2" label="Food" label-for="example-input-2">
+                        <b-form-select
+                                id="example-input-2"
+                                name="example-input-2"
+                                v-model="form.food"
+                                :options="foods"
+                                :state="getValidationState(validationContext)"
+                                aria-describedby="input-2-live-feedback"
+                        ></b-form-select>
+
+                        <b-form-invalid-feedback id="input-2-live-feedback">{{ validationContext.errors[0] }}
+                        </b-form-invalid-feedback>
+                    </b-form-group>
+                </validation-provider>
+
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button class="ml-2" @click="resetForm()">Reset</b-button>
+            </b-form>
+        </validation-observer>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data() {
+            return {
+                foods: [
+                    {value: null, text: "Choose..."},
+                    {value: "apple", text: "Apple"},
+                    {value: "orange", text: "Orange"}
+                ],
+                form: {
+                    name: null,
+                    food: null
+                }
+            };
+        },
+        methods: {
+            getValidationState({dirty, validated, valid = null}) {
+                return dirty || validated ? valid : null;
+            },
+            resetForm() {
+                this.form = {
+                    name: null,
+                    food: null
+                };
+
+                this.$nextTick(() => {
+                    this.$refs.observer.reset();
+                });
+            },
+            onSubmit() {
+                alert("Form submitted!");
+            }
+        }
     }
 </script>
 
 <style scoped>
+    body {
+        padding: 1rem;
+    }
 
 </style>
