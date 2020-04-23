e7cd2856a6e0851ff1a45c3ea7a7b3e811c81bff
sergey <lith2009@mail.ru>
2020-04-23 21:14:50 +0300

    all right

--- frontend/src/templates/Successful.vue
+++ frontend/src/templates/Successful.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="orderSuccessful">
-        <p>Ваша заявка принята! Спасибо за покупку!</p>
-        <b-button variant="outline-primary" @click="backToMain">Вернуться на главную страницу</b-button>
+    <div class="container">
+        <div class="orderSuccessful">
+            <p>Ваша заявка принята! Спасибо за покупку!</p>
+            <b-button variant="outline-primary" @click="backToMain">Вернуться на главную страницу</b-button>
+        </div>
     </div>
+
 </template>
 
 <script>
     export default {
         name: "Successful",
         methods: {
-              backToMain() {
+            backToMain() {
                 this.$router.go(-3);
             }
         }
@@ -19,6 +22,11 @@
 <style scoped>
     .orderSuccessful {
         text-align: center;
+        margin: 20px 0;
+        padding: 15px;
+        border: 1px solid green;
+        background-color: #f2f2f2;
+        border-radius: 3px;
     }
 
     p {
